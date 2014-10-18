7744075f16a4a3bef1e82c1b43dc25e8a4293a2c
Georgy <georgynet@gmail.com>
2014-10-18 21:32:32 +0400

    [migration] Add migration

---
+++ console/migrations/m141018_161427_18102014.php
@@ -0,0 +1,47 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m141018_161427_18102014 extends Migration
+{
+    public function up()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('{{%category}}', [
+            'id' => Schema::TYPE_PK,
+            'title' => Schema::TYPE_STRING . ' NOT NULL',
+        ], $tableOptions);
+
+        $this->createTable('{{%post}}', [
+            'id' => Schema::TYPE_PK,
+            'title' => Schema::TYPE_STRING . ' NOT NULL',
+            'anons' => Schema::TYPE_TEXT . ' NOT NULL',
+            'content' => Schema::TYPE_TEXT . ' NOT NULL',
+            'category_id' => Schema::TYPE_INTEGER,
+            'author_id' => Schema::TYPE_INTEGER,
+            'publish_status' => "enum('draft','publish') NOT NULL DEFAULT 'draft'",
+            'publish_date' => Schema::TYPE_TIMESTAMP . ' NOT NULL',
+        ], $tableOptions);
+
+        $this->createIndex('FK_post_author', '{{%post}}', 'author_id');
+        $this->addForeignKey(
+            'FK_post_author', '{{%post}}', 'author_id', '{{%user}}', 'id', 'SET NULL', 'CASCADE'
+        );
+
+        $this->createIndex('FK_post_category', '{{%post}}', 'category_id');
+        $this->addForeignKey(
+            'FK_post_category', '{{%post}}', 'category_id', '{{%category}}', 'id', 'SET NULL', 'CASCADE'
+        );
+    }
+
+    public function down()
+    {
+        $this->dropTable('{{%post}}');
+        $this->dropTable('{{%category}}');
+    }
+}
