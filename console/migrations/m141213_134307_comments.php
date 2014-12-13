af5dc18cba70ef5d9110f1701a669ada4359214b
Georgy Shpak <georgynet@gmail.com>
2014-12-13 17:18:52 +0300

    [comment] Fix migration. Create models

--- console/migrations/m141213_134307_comments.php
+++ console/migrations/m141213_134307_comments.php
@@ -1,5 +1,6 @@
 <?php
 
+use common\models\Comment;
 use yii\db\Schema;
 use yii\db\Migration;
 
@@ -14,8 +15,10 @@ class m141213_134307_comments extends Migration
 
         $this->createTable('{{%comment}}', [
             'id' => Schema::TYPE_PK,
+            'pid' => Schema::TYPE_INTEGER,
             'title' => Schema::TYPE_STRING . ' NOT NULL',
             'content' => Schema::TYPE_STRING . ' NOT NULL',
+            'publish_status' => "enum('" . Comment::STATUS_MODERATE . "','" . Comment::STATUS_PUBLISH . "') NOT NULL DEFAULT '" . Comment::STATUS_MODERATE . "'",
             'post_id' => Schema::TYPE_INTEGER,
             'author_id' => Schema::TYPE_INTEGER
         ], $tableOptions);
