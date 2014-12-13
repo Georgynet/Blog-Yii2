5b1e34b739d776b570b409c60bc410592ce51a9f
Georgy Shpak <georgynet@gmail.com>
2014-12-13 22:44:02 +0300

    [admin] Add CRUD

---
+++ backend/views/comment/view.php
@@ -0,0 +1,41 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Comment */
+
+$this->title = $model->title;
+$this->params['breadcrumbs'][] = ['label' => 'Comments', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="comment-view">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
+            'class' => 'btn btn-danger',
+            'data' => [
+                'confirm' => 'Are you sure you want to delete this item?',
+                'method' => 'post',
+            ],
+        ]) ?>
+    </p>
+
+    <?= DetailView::widget([
+        'model' => $model,
+        'attributes' => [
+            'id',
+            'pid',
+            'title',
+            'content',
+            'publish_status',
+            'post_id',
+            'author_id',
+        ],
+    ]) ?>
+
+</div>
