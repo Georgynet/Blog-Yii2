b818cda00d5c6a5048b57055da64592ad1c3df7c
Georgy Shpak <georgynet@gmail.com>
2015-02-21 13:40:46 +0300

    [frontend] Change defaultRoute

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -10,6 +10,7 @@ return [
     'id' => 'app-frontend',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'defaultRoute' => 'post/index',
     'controllerNamespace' => 'frontend\controllers',
     'components' => [
         'request' => [
